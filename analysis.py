1e5baab675369035b5e382218e5632ce3aa4f05f
liligould <liligould1564@gmail.com>
2021-07-31 14:56:58 -0400

    Adding file

---
+++ analysis.py
@@ -0,0 +1,19 @@
+#  Predicting World Happiness
+## Overview
+The selected topic for the project is predicting world happiness utilizing the world happiness dataset from Kaggle.  This dataset consists of numerous continuous variables presenting the opportunity to utilize a multiple regression machine learning model to predict the happiness of a country or region of the world.  Additionally, using backwards elimination, the variables that are the least predictive of the happiness score can be removed to improve the performance of the model
+The source data includes the following:
+Country (categorical)
+Region of the World (categorical)
+Year (categorical)
+Happiness Score (continuous)
+Economic Production (continuous)
+Social Support (continuous)
+Life Expectancy (continuous)
+Freedom (continuous)
+Absence of Corruption (continuous)
+Generosity (continuous)
+
+In this project, we are aiming to answer the following questions:
+What variables are most predictive of happiness by country?
+What variables are most predictive of happiness by region of the world?
+Which region’s happiness has gone up the most? Which has gone down?
